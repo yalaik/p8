41cc4cf18efe88f2381d630e9fb33a31f8a4664b
yalaik <oduur1@ya.ru>
2024-07-09 01:58:01 +0500

    Function minus is added

--- first/num.py
+++ first/num.py
@@ -1,5 +1,11 @@
 def plus(a, b):
     return a+b
 
+text = 'new text'
+
+def minus(a, b):
+    return a-b
+
 if __name__ == '__main__':
-    print (plus(2, 2))
+    print(plus(2, 2))
+    print(minus(4,2))
