564a08948a90666e0b5d8682c3dbab46cb4371db
yalaik <oduur1@ya.ru>
2024-07-09 02:08:24 +0500

    Function mul is added. Module string.py is added. Text is deleted.

--- first/num.py
+++ first/num.py
@@ -1,7 +1,8 @@
 def plus(a, b):
     return a+b
 
-text = 'new text'
+def mul(a,b):
+    return a*b
 
 def minus(a, b):
     return a-b
@@ -9,3 +10,4 @@ def minus(a, b):
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4,2))
+    print(mul(3, 3))
